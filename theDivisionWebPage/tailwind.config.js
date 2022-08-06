606c4bd18025c514b1b54c5ccdaf974227c766ba
Thomas Villafranco <zonick_23@hotmail.com>
2022-08-06 16:36:50 -0500

    terminado el home del proyecto

---
+++ theDivisionWebPage/tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**.html",
+    
+  ],
+  
+  theme: {
+    extend: {
+      colors: {
+        "primary": "#ff6c0f",
+        "secondary": "#060000",
+      },
+      height: {
+        "80": "20rem",
+        "100": "25rem",
+        "120": "30rem",
+        "140": "35rem",
+        "160": "40rem",
+
+      }
+    },
+  },
+    plugins: []
+      
+    
+  
+}
